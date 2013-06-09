205bb46437bd0b4d5459b24d38b80f277ec7c39c
zakuni <kunio038@gmail.com>
2013-06-09 19:35:21 +0900

    add main.js and tests.

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var assert = chai.assert;
+
 suite('Array', function(){
   setup(function(){
     // ...
@@ -8,5 +9,13 @@ suite('Array', function(){
     test('should return -1 when not present', function(){
       assert.equal(-1, [1,2,3].indexOf(4));
     });
+  });
+});
+
+suite('main', function(){
+  suite('a', function(){
+  	test('should be 1', function(){
+  	  assert.equal(1, a);
+  	});
   });
 });
